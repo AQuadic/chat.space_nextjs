fd0013c2d6d0b2defafaf28daacd293d590b9cca
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-08-05 23:07:11 +0300

    solutions hero

--- src/components/solution/MainSolution.tsx
+++ src/components/solution/MainSolution.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import SolutionBrands from './SolutionBrands'
 import Testimonials from '../general/Testimonials'
 import DiscoverMore from '../general/DiscoverMore'
+import SolutionHero from './SolutionHero'
 
 const MainSolution = () => {
     return (
         <div>
+            <SolutionHero />
             <SolutionBrands />
             <Testimonials />
             <DiscoverMore />

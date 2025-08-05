976058c6e33d532d9e7ea19074670c41a3c7c342
ahmed <ahmedabozaid.dev@gmail.com>
2025-08-05 14:09:21 +0300

    adds project structure

---
+++ src/components/home/MainHome.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeHero from "@/components/home/HomeHero";
+
+const MainHome = () => {
+  return (
+    <div>
+      <HomeHero />
+    </div>
+  );
+};
+
+export default MainHome;

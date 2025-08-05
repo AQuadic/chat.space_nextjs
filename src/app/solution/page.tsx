bf33a9378c7eb996d1e0a3e62d44552a30ffcb1e
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-08-05 22:53:04 +0300

    add solution page - brands,testimonials and discover more

---
+++ src/app/solution/page.tsx
@@ -0,0 +1,12 @@
+import MainSolution from '@/components/solution/MainSolution'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <MainSolution />
+        </div>
+    )
+}
+
+export default page
---
+++ src/components/solution/MainSolution.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import SolutionBrands from './SolutionBrands'
+import Testimonials from '../general/Testimonials'
+import DiscoverMore from '../general/DiscoverMore'
+
+const MainSolution = () => {
+    return (
+        <div>
+            <SolutionBrands />
+            <Testimonials />
+            <DiscoverMore />
+        </div>
+    )
+} 
+
+export default MainSolution

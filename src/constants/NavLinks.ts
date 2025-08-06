3583e179203cb07c7ff7fd5d9f7b8c03979616bf
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-08-06 11:31:11 +0300

    add features dropdown

--- src/constants/NavLinks.ts
+++ src/constants/NavLinks.ts
@@ -6,6 +6,12 @@ export const NavLinks = [
   {
     title: "Features",
     path: "/features",
+    subLinks: [
+      { title: "Broadcast & Bulk Messages", path: "/features/broadcast" },
+      { title: "Shared Team Inbox", path: "/features/inbox" },
+      { title: "Custom Notifications", path: "/features/notifications" },
+      { title: "Multiple WhatsApp Numbers", path: "/features/numbers" },
+    ],
   },
   {
     title: "Solution",

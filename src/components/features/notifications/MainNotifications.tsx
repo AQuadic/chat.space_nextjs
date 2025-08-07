f868a2327f592eb8e78ec767b2b114e477e6545b
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-08-07 15:18:47 +0300

    add notification title section

--- src/components/features/notifications/MainNotifications.tsx
+++ src/components/features/notifications/MainNotifications.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import NotificationsHeader from "./NotificationsHeader";
+import NotificationTitle from "./NotificationTitle";
 
 const MainNotifications = () => {
   return <div>
     <NotificationsHeader />
+    <NotificationTitle />
   </div>;
 };
 
---
+++ src/components/features/notifications/NotificationTitle.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotificationTitle = () => {
+    return (
+        <section className='bg-[#FFF5DC]'>
+            <div className='container flex items-center justify-center py-8'>
+                <h2 className='text-[#1F2937] md:text-[30px] text-base font-semibold text-center'>Timely information for users, teams and contacts</h2>
+            </div>
+        </section>
+    )
+}
+
+export default NotificationTitle

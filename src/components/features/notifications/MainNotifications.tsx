b33be6c777a5d99d6c2a33a85940c5e07f843aca
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-08-07 16:20:25 +0300

    Notifications Features section

--- src/components/features/notifications/MainNotifications.tsx
+++ src/components/features/notifications/MainNotifications.tsx
@@ -3,12 +3,14 @@ import NotificationsHeader from "./NotificationsHeader";
 import NotificationTitle from "./NotificationTitle";
 import DiscoverMore from "@/components/general/DiscoverMore";
 import NotificationCapabilities from "./NotificationCapabilities";
+import NotificationsFeatures from "./NotificationsFeatures";
 
 const MainNotifications = () => {
   return <div>
     <NotificationsHeader />
     <NotificationCapabilities />
     <NotificationTitle />
+    <NotificationsFeatures />
     <DiscoverMore />
   </div>;
 };
---
+++ src/components/features/notifications/NotificationsFeatures.tsx
@@ -0,0 +1,74 @@
+import SolutionCheck from '@/components/icons/solution/SolutionCheck'
+import Image from 'next/image'
+import React from 'react'
+import featureBG1 from '../../../../public/images/features/featureBG1.png'
+import featureBG2 from '../../../../public/images/features/featureBG2.png'
+import featureBG3 from '../../../../public/images/features/featureBG3.png'
+
+const NotificationsFeatures = () => {
+    return (
+        <section className='container py-16'>
+            <div className='flex flex-wrap items-center gap-4 justify-between'>
+                <div>
+                    <h1 className='text-[#111827] text-[30px] font-semibold font-[Poppins] leading-[100%]'>Update customers and prospects</h1>
+                    <ul className='mt-9'>
+                        <li className='flex items-center gap-3'>
+                            <SolutionCheck />
+                            <p className='text-[#4B5563] text-base font-normal leading-[150%]'>Alert customers when products are in stock</p>
+                        </li>
+                        <li className='flex items-center gap-3 mt-4'>
+                            <SolutionCheck />
+                            <p className='text-[#4B5563] text-base font-normal leading-[150%]'>Cross-sell based on interests</p>
+                        </li>
+                    </ul>
+                </div>
+                <div className='relative'>
+                    <Image src='/images/features/notificationfeature1.png' alt='image' width={557} height={370} className='z-10' />
+                    <Image src={featureBG1} alt='bg' className='absolute top-0' width={704} />
+                </div>
+            </div>
+
+            <div className='flex flex-wrap items-center gap-4 justify-between mt-20'>
+                <div className='relative'>
+                    <Image src='/images/features/notificationfeature2.png' alt='image' width={592} height={320} className='z-10' />
+                    <Image src={featureBG2} alt='bg' className='absolute top-0 -z-10' width={704} />
+                </div>
+                <div>
+                    <h1 className='text-[#111827] text-[30px] font-semibold font-[Poppins] leading-[100%]'>Update customers and prospects</h1>
+                    <ul className='mt-9'>
+                        <li className='flex items-center gap-3'>
+                            <SolutionCheck />
+                            <p className='text-[#4B5563] text-base font-normal leading-[150%]'>Alert customers when products are in stock</p>
+                        </li>
+                        <li className='flex items-center gap-3 mt-4'>
+                            <SolutionCheck />
+                            <p className='text-[#4B5563] text-base font-normal leading-[150%]'>Cross-sell based on interests</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className='flex flex-wrap items-center gap-4 justify-between mt-20'>
+                <div>
+                    <h1 className='text-[#111827] text-[30px] font-semibold font-[Poppins] leading-[100%]'>Update customers and prospects</h1>
+                    <ul className='mt-9'>
+                        <li className='flex items-center gap-3'>
+                            <SolutionCheck />
+                            <p className='text-[#4B5563] text-base font-normal leading-[150%]'>Alert customers when products are in stock</p>
+                        </li>
+                        <li className='flex items-center gap-3 mt-4'>
+                            <SolutionCheck />
+                            <p className='text-[#4B5563] text-base font-normal leading-[150%]'>Cross-sell based on interests</p>
+                        </li>
+                    </ul>
+                </div>
+                <div className='relative'>
+                    <Image src='/images/features/notificationfeature3.png' alt='image' width={544} height={343} className='z-10' />
+                    <Image src={featureBG3} alt='bg' className='absolute top-0' width={704} />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default NotificationsFeatures

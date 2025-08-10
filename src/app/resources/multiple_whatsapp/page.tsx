6637127d05b5f75c72693b05fdbbc93c1293b352
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-08-10 12:21:45 +0300

    last section - resources

--- src/app/resources/multiple_whatsapp/page.tsx
+++ src/app/resources/multiple_whatsapp/page.tsx
@@ -1,5 +1,6 @@
 import TrustedBrands from '@/components/home/TrustedBrands'
 import Features from '@/components/resources/multiple_whatsapp/Features'
+import FreeTrial from '@/components/resources/multiple_whatsapp/FreeTrial'
 import MultipleWhatsappHero from '@/components/resources/multiple_whatsapp/MultipleWhatsappHero'
 import TheProblem from '@/components/resources/multiple_whatsapp/TheProblem'
 import React from 'react'
@@ -14,6 +15,7 @@ const page = () => {
         />
         <TheProblem />
         <Features />
+        <FreeTrial />
     </div>
   )
 }
---
+++ src/components/resources/multiple_whatsapp/FreeTrial.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FreeTrial = () => {
+    return (
+        <section className='container bg-[#DCFCE7] rounded-2xl py-16 flex flex-col items-center mb-10'>
+            <h1 className='text-[#000000] lg:text-4xl text-2xl font-bold leading-[100%] text-center'>Ready to Simplify Your WhatsApp Operations?</h1>
+            <button className='w-[235px] h-[60px] bg-[#10B981] rounded-[8px] mt-9 text-[#FFFFFF] text-lg font-semibold'>
+                Start 7 day free trial
+            </button>
+        </section>
+    )
+}
+
+export default FreeTrial

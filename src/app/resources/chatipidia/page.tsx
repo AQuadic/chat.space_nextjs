979b880b0438cf71fed916c94db20a26fdea9e2c
ahmed <ahmedabozaid.dev@gmail.com>
2025-08-10 12:00:31 +0300

    adds chatipedia header

---
+++ src/app/resources/chatipidia/page.tsx
@@ -0,0 +1,8 @@
+import MainChatipidia from "@/components/resources/chatipidia/MainChatipidia";
+import React from "react";
+
+const Page = () => {
+  return <MainChatipidia />;
+};
+
+export default Page;

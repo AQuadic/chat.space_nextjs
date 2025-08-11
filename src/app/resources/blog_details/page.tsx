4b2c31946ce06dcf6d7c589e85237d019311b45b
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-08-11 13:10:06 +0300

    blog deatils page - second section

--- src/app/resources/blog_details/page.tsx
+++ src/app/resources/blog_details/page.tsx
@@ -1,10 +1,12 @@
 import BlogDetailsHero from '@/components/resources/blog_details/BlogDetailsHero'
+import BroadcastCount from '@/components/resources/blog_details/BroadcastCount'
 import React from 'react'
 
 const page = () => {
     return (
         <div>
             <BlogDetailsHero />
+            <BroadcastCount />
         </div>
     )
 }
---
+++ src/components/resources/blog_details/BroadcastCount.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const BroadcastCount = () => {
+    return (
+        <section className='bg-[linear-gradient(90deg,#25D366_0%,#16A34A_100%)] py-12'>
+            <div className='container flex flex-wrap items-center justify-between'>
+                <div>
+                    <h1 className='text-[#FFFFFF] md:text-[30px] text-xl text-center font-bold leading-[100%]'>Make Every WhatsApp Broadcast Count!</h1>
+                    <p className='text-[#DCFCE7] md:text-lg text-base font-normal leading-[150%] mt-6 capitalize'>Avoid blocks with our professional WhatsApp automation platform. Send messages safely and efficiently.</p>
+                </div>
+                <button className='w-[128px] h-12 bg-[#FFFFFF] rounded-[8px] text-[#25D366] text-base font-semibold font-[Inter] lg:mt-0 mt-4'>
+                    Try Now
+                </button>
+            </div>
+        </section>
+    )
+}
+
+export default BroadcastCount
